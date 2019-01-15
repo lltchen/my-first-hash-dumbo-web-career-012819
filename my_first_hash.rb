04c5bfae201c73bde7b378cb5f30f4d56a60e9fc
lltchen <lltchen1@gmail.com>
2019-01-15 21:01:03 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,5 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+  money_for = {"GAS" => 10, "RENT" => 200}
   
 end
 
