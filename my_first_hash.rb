6cc1fdd48fe2bda254860e38970dbdbc6359cae4
lltchen <lltchen1@gmail.com>
2019-01-15 21:36:57 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -17,10 +17,10 @@ def retrieval
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
-
-  shipping_manifest["oil paintings"]
-
 end
+
+shipping_manifest["oil paintings"]
+
 
 def adding
   shipping_manifest = {
